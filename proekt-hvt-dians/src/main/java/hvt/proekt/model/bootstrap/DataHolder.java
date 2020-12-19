d0ad8180211c24c13811641dd34afc7a0f9e85a1
hristijanpeshov <67428742+hristijanpeshov@users.noreply.github.com>
2020-12-19 19:56:33 +0100

    Added distance, changes to search and list objects

--- proekt-hvt-dians/src/main/java/hvt/proekt/model/bootstrap/DataHolder.java
+++ proekt-hvt-dians/src/main/java/hvt/proekt/model/bootstrap/DataHolder.java
@@ -17,13 +17,4 @@ public class DataHolder {
     public static File bankFile = new File("src/main/java/hvt/proekt/model/bootstrap/db/bank.csv");;
     public static File exchangeFile = new File("src/main/java/hvt/proekt/model/bootstrap/db/exchange.csv");;
 
-//    @PostConstruct
-//    public void init(){
-//        services.add(new MoneyObject( 0L, Type.ATM, new Location(41.9936657, 21.4428736), "НБРМ"));
-//        services.add(new MoneyObject( 1L, Type.ATM, new Location(41.9921343,21.4266335), "Шпаркасе"));
-//        services.add(new MoneyObject( 2L, Type.BANK, new Location(41.9936657,21.4428736), "НБРМ"));
-//        services.add(new MoneyObject( 3L, Type.BANK, new Location(41.9881468,21.4552843), "Стопанска банка"));
-//        services.add(new MoneyObject( 4L, Type.EXCHANGE, new Location(41.9969234,21.4383163), "Exchange 1"));
-//        services.add(new MoneyObject( 5L, Type.EXCHANGE, new Location(41.9869314,21.4308515), "Exchange 2"));
-//    }
 }
